ec9f3e78fb703d502ec2ec9013ed6f3e051ab4ef
Mick Ahlmann Brun <mick@mickbrun.com>
2023-01-13 11:50:44 +0100

    Add structure to lab

---
+++ Labs/Danish/Lab_Window-funktioner_KOMMENTERET.sql
@@ -0,0 +1,114 @@
+/* ***********************
+
+Kursus: SQL Basis 2
+Emne: Window-funktioner
+Version: 1.0
+Dato: 2023-01-11
+
+Laboratoriet kræver:
+
+- En understøttet version af SQL Server
+- En Stack Overflow database: https://www.BrentOzar.com/go/querystack
+
+Agenda:
+
+- Introduktion til konceptet om window-funktioner, deres opbygning og delsætninger
+- Introduktion til forskellige familier af window-funktioner
+- Eksempler på løsningsmønstre med window-funktioner
+
+Læs mere om window funktioner i Microsofts T-SQL reference:
+
+- https://learn.microsoft.com/en-us/sql/t-sql/queries/select-over-clause-transact-sql?view=sql-server-ver16
+
+*/
+
+USE StackOverflow2013;
+GO
+
+/* ***********************
+
+Introduktion til window-funktioner:
+
+- Windowing bruges som et effektivt og fleksibelt værktøj til analytiske formål
+- Window-funktioner er, i sin essens, nogle funktioner som for hver række laver en beregning over en mængde
+    af rækker relateret til den aktuelle række
+- Mængden af rækker som der laves en beregning henover, kaldes et vindue ("window")
+- Modsat vores grupperede forespørgsler, så kan vi med window-funktioner bevare detaljen. Derudover
+    kan man også definere en orden i sin beregning
+
+*/
+
+/* [Vis eksempler som illustrerer hvordan window-funktioner fungerer vis-a-vis grupperede forespørgsler] */
+
+/* [Mockup] */
+
+/* [Stack Overflow] */
+
+/* ***********************
+
+Opbygningen af window-funktioner:
+
+- Window Partitioning
+- Window Ordering
+- Window Framing
+
+*/
+
+/* [Kommentar] */
+
+/* [Mockup] */
+
+/* [Stack Overflow] */
+
+/* ***********************
+
+Logisk query processering af window-funktioner:
+
+- Hvor evalueres window-funktioner?
+- Hvilke delsætninger understøtter window-funktioner?
+- Hvordan tages window-funktioner i brug i andre delsætninger?
+
+*/
+
+/* [Kommentar] */
+
+/* [Mockup] */
+
+/* [Stack Overflow] */
+
+/* ***********************
+
+Introduktion til familier af window-funktioner:
+
+- Aggregering
+- Ranking
+- Offset
+- Statistik
+
+*/
+
+/* [Kommentar] */
+
+/* [Mockup] */
+
+/* [Stack Overflow] */
+
+/* ***********************
+
+Løsningsmønstre med window-funktioner:
+
+*/
+
+/* [Kommentar] */
+
+/* [Mockup] */
+
+/* [Stack Overflow] */
+
+/* ***********************
+
+Hovedpointer:
+
+- 
+
+*/
