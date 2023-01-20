c4871eb560ec34518b8c8f77cf6705fcfe9d3ff3
Thomas Lange <thomas@langegruppen.dk>
2023-01-20 23:32:51 +0100

    PIVOT refined and exercises added

--- Labs/Danish/Lab_PIVOT_og_UNPIVOT_KOMMENTERET.sql
+++ Labs/Danish/Lab_PIVOT_og_UNPIVOT_KOMMENTERET.sql
@@ -48,6 +48,13 @@ Use case(s):
 - Pivotering til præsentationsformål, her bruges særligt PIVOT (resultatet bliver til en "crosstab")
 - Normalisering af en tabel, her bruges særligt UNPIVOT
 
+Pivotering handler altid om at identificere involverede elementer: 
+  Grupperingselementet
+  Spredningselementet 
+  Aggregeringselementet 
+  Aggregeringsfunktionen
+
+
 */
 
 /* [Vis, hvordan tabel-resultater fra en SELECT-forespørgsel kan vendes 
@@ -57,54 +64,157 @@ Use case(s):
 
 /* [Mockup] */
 
-CREATE TABLE #TableA (
-  Id int NOT NULL,
-  GrpCol nvarchar(1) NOT NULL, 
-  SumCol int NOT NULL
+CREATE TABLE #TableTxt (
+  GroupId int NOT NULL,
+  SpreadCol nvarchar(1) NOT NULL, 
+  AggCol int NOT NULL
 );
-
-INSERT INTO #TableA (Id, GrpCol, SumCol)
+INSERT INTO #TableTxt (GroupId, SpreadCol, AggCol)
 VALUES
 (1, 'A', 100), 
 (1, 'A', 100), 
 (2, 'A', 200), 
 (2, 'B', 300);
 
-SELECT * FROM #TableA;
+CREATE TABLE #TableNum (
+  GroupId int NOT NULL,
+  SpreadCol int NOT NULL, 
+  AggCol int NOT NULL
+);
+INSERT INTO #TableNum (GroupId, SpreadCol, AggCol)
+VALUES
+(1, 10, 100), 
+(1, 10, 100), 
+(2, 10, 200), 
+(2, 20, 300);
 
---Hvis vi skulle gøre det 'i hånden'...
-SELECT Id, 
- SUM(CASE WHEN GrpCol = 'A' THEN SumCol END) AS A, 
- SUM(CASE WHEN GrpCol = 'B' THEN SumCol END) AS B
-FROM #TableA 
-GROUP BY Id
+SELECT * FROM #TableTxt;
+SELECT * FROM #TableNum;
 
---Med PIVOT bliver syntaksen mere kompakt...
-SELECT Id, A, B
-FROM #TableA
-PIVOT( SUM(SumCol) FOR GrpCol IN (A,B) ) AS p
 
-DROP TABLE #TableA;
+--Hvad er grupperingselementet? Id
+--Hvad er spredningselementet? GrpCol
+--Hvad er aggregeringselementet? SumCol
+--Hvad er aggregeringsfunktionen? SUM()
+
+
+--Hvis vi skulle gøre det 'i hånden' ...
+--med tekst spredningskolonne
+SELECT GroupId, 
+ SUM(CASE WHEN SpreadCol = 'A' THEN AggCol END) AS A, 
+ SUM(CASE WHEN SpreadCol = 'B' THEN AggCol END) AS B
+FROM #TableTxt 
+GROUP BY GroupId;
+
+--med numerisk spredningskolonne...
+SELECT GroupId, 
+ SUM(CASE WHEN SpreadCol = 10 THEN AggCol END) AS [10], 
+ SUM(CASE WHEN SpreadCol = 20 THEN AggCol END) AS [20]
+FROM #TableNum 
+GROUP BY GroupId;
+
+
+--Med PIVOT bliver syntaksen mere kompakt og ens for tekst og numeriske spredningskolonner...
+
+SELECT GroupId, [A], [B]
+FROM #TableTxt
+PIVOT( SUM(AggCol) FOR SpreadCol IN ([A],[B]) ) AS p;
+
+SELECT GroupId, [10], [20]
+FROM #TableNum
+PIVOT( SUM(AggCol) FOR SpreadCol IN ([10],[20]) ) AS p;
+
+DROP TABLE #TableTxt;
+DROP TABLE #TableNum;
 
 /* [Stack Overflow] */
 
-
-
 -- Posts indeholder 1 række pr. post med angivet posttype, creationdate mv., samt optællinger af svar og kommentarer mv.:
-SELECT TOP 10 * FROM dbo.posts 
+SELECT TOP 10 * FROM dbo.posts;
 -- Vi vil her kun bruge PostCreationYear, PostTypeId og Commentcount for årene 2008-2010:
 SELECT TOP 10 
-  YEAR(CreationDate) AS PostCreationYear, PostTypeId, CommentCount FROM dbo.posts WHERE CreationDate BETWEEN CAST('20080101' AS date) AND CAST('20101231' AS date)
+  YEAR(CreationDate) AS PostCreationYear, PostTypeId, CommentCount FROM dbo.posts WHERE CreationDate BETWEEN CAST('20080101' AS date) AND CAST('20101231' AS date);
 
---Vil nu gerne optælle summen af Questions og Answers pr PostCreationYear
+
+--Vil nu for hvert PostCreationYear optælle summen af CommentCount for hver af PostTypeId-værdierne som repræsenterer Questions og Answers
+
+--Hvad er grupperingselementet? PostCreationYear
+--Hvad er spredningselementet? PostTypeId
+--Hvad er aggregeringselementet? CommentCount
+--Hvad er aggregeringsfunktionen? SUM()
+
 SELECT p.PostCreationYear, [1] AS Questions, [2] AS Answers
 FROM (
   SELECT YEAR(CreationDate) AS PostCreationYear, PostTypeId, CommentCount FROM dbo.posts WHERE CreationDate BETWEEN CAST('20080101' AS date) AND CAST('20101231' AS date)
   ) posts
-PIVOT ( SUM(CommentCount) FOR PostTypeId IN ([1],[2])) AS p
+PIVOT ( SUM(CommentCount) FOR PostTypeId IN ([1],[2])) AS p;
+
+
+
+/* [I opgaverne skal man lave beregninger a la eksemplerne, med og uden brug af PIVOT-operatoren. Det skal
+    gerne illustrere effektiv og kompakt syntaksen bliver med PIVOT] */
+
+/*
+Opgave 1
+
+Dette script danner en temporær tabel, #postbyusertype, som indeholder fire kolonner for poster, som er lukkede:
+  PostClosedYear = Året hvor posten i dbo.Posts lukkedes
+  UserType = 'Superuser' hvis Reputation i dbo.Users >= 10.000; 'User' ellers
+  ViewCount = Antal visninger af posten i dbo.Posts */
+
+
+SELECT YEAR(p.ClosedDate) AS PostClosedYear, CASE WHEN u.Reputation>=10000 THEN 'Superuser' ELSE 'User' END AS UserType, p.ViewCount
+INTO #viewsbyusertypes
+FROM dbo.Posts p 
+INNER JOIN dbo.Users u ON u.Id=p.OwnerUserId
+WHERE ClosedDate IS NOT NULL ;
+
+/*
+Du skal optælle det gennemsnitlige antal visninger for Users og Superusers fordelt på antal år med brug af PIVOT-operatoren. 
+
+1.a. Hvad er 
+  Grupperingselementet?
+  Spredningselementet? 
+  Aggregeringselementet? 
+  Aggregeringsfunktionen?
+
+1.b. Skriv en forespørgsel med brug af PIVOT-operatoren, som danner det ønskede output
+1.c. Kig på resultatet. Hvad kan man konkludere om sammenhængen mellem reputation og visninger?
+
+
+- Tabeller involveret:  #postsbyusertype (eller, om vil dbo.Posts og dbo.Users)
+- Ønsket output:
+
+PostClosedYear	User	Superuser
+2008	7895	5460
+2009	9876	13478
+2010	4934	11035
+2011	5348	9442
+2012	4754	11972
+2013	4309	10487
+2014	6283	21185
+2015	14236	19902
+2016	22574	32564
+2017	16269	24736
+2018	18527	27985
+
+*/
+
+/*
+Svar på opgaven:
+*/
+SELECT PostClosedYear, [User], [Superuser]
+FROM #viewsbyusertypes
+PIVOT(AVG(ViewCount) FOR UserType IN ([User],[Superuser])) p
+
+DROP TABLE #viewsbyusertypes;
+*/
+
 
 /* ***********************
 
 Hovedpointer:
+
+
 
 */
