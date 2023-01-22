7f1778ece9a445fec0aad66141679fe3557a323a
Mick Ahlmann Brun <mick@mickbrun.com>
2023-01-22 20:55:40 +0100

    Remove exercises from lab

--- Laboratories/Danish/Lab_SET-operatorer_kommenteret.sql
+++ Laboratories/Danish/Lab_SET-operatorer_kommenteret.sql
@@ -127,56 +127,10 @@ WHERE UpVotes>=5000;
 --[UNION ALL 2083 rows]
 
 /*
-Opgave 1: Hvad er forskellen på UNION og UNION ALL?
-   Hvornår er de ens?
+
+Lav opgave 1, 2 (optional) og 3
+
 */
-
-/* [Forskellen på UNION og UNION ALL er, UNION returnerer distinkte rækker, mens UNION ALL returnerer rækker 
-  det samlede antal gange, de findes i SELECT-forespørgslernes resultat-tabeller.]*/
-
-/*  
-Opgave 2 (optional): Nogle gange kan du bruge både FULL OUTER JOIN og UNION ALL til at løse en opgave. Hvornår vælger du UNION/UNION ALL?
-*/
-
-/* [UNION og UNION ALL bruger og sammenligner alle kolonner defineret i SELECT-forespørgslerne, uden at 
-  der skal defineres en lang liste af betingelser for sammenligninger (som der skal i FULL OUTER JOINs).
-  Ofte er det derfor simplere at lave UNION eller UNION ALL end et FULL OUTER JOIN */
-
-/*
-Opgave 3: Skriv en forespørsel som giver dig tallene fra 1 til 10. 
-- Tabeller involveret: Ingen
-- Ønsket output:
-   n
-   -----------
-   1
-   2
-   3
-   4
-   5
-   6
-   7
-   8
-   9
-   10
-*/
-
--- Med UNION ALL:
-SELECT 1 AS n
-UNION SELECT 2
-UNION SELECT 3
-UNION SELECT 4
-UNION SELECT 5
-UNION SELECT 6
-UNION SELECT 7
-UNION SELECT 8
-UNION SELECT 9
-UNION SELECT 10;
-
--- Med VALUES:
-SELECT n
-FROM (VALUES(1),(2),(3),(4),(5),(6),(7),(8),(9),(10)) AS Nums(n);
-
-
 
 /* ***********************
 
@@ -282,55 +236,11 @@ SELECT
 FROM dbo.Users
 WHERE [Location] LIKE '%copenhagen%';
 
-
 /*
-Opgave 4: Skriv en forespørgsel som finder distinkte brugere i StackOverflow
- , som har givet DownVotes, men ingen UpVotes. 
-- Tabeller involveret: dbo.Users
-- Ønsket output: 
-Id	  DisplayName
-211	  jhornnes
-15299	user15299
-17020	Matt
-17060	user17060
-... (495 rows)
+
+Lav opgave 4 og 5
+
 */
-
-SELECT Id,DisplayName
-FROM dbo.Users
-WHERE DownVotes>0
-
-EXCEPT 
-
-SELECT Id,DisplayName
-FROM dbo.Users
-WHERE UpVotes>0;
-
-
-
-/*
-Opgave 5: Skriv en forespørgsel som finder distinkte brugere i StackOverflow
- , som har givet mindst 1000 DownVotes og mindst 10000 UpVotes. 
-- Tabeller involveret: dbo.Users
-- Ønsket output: 
-Id	  DisplayName
--1	  Community
-476	  deceze
-1288	Bill the Lizard
-3043	Joel Coehoorn
-... (79 rows)
-*/
-
-SELECT Id,DisplayName
-FROM dbo.Users
-WHERE DownVotes>1000
-
-INTERSECT 
-
-SELECT Id,DisplayName
-FROM dbo.Users
-WHERE UpVotes>10000;
-
 
 /* ***********************
 
