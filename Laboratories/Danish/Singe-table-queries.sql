94dd4637ef3fd261f180226a9417500c227e78ea
Mick Ahlmann Brun <mick@mickbrun.com>
2023-02-09 14:34:08 +0100

    Add templates for missing sections

---
+++ Laboratories/Danish/Singe-table-queries.sql
@@ -0,0 +1,75 @@
+/* ***********************
+
+SINGLE TABLE-QUERIES
+
+Udviklet af Thomas Lange & Mick Ahlmann Brun
+
+Mere info: https://github.com/M1ckB/T-SQL
+
+Version 1.0 2023-02-09
+
+Laboratoriet kræver:
+
+- En understøttet version af SQL Server
+- En Stack Overflow database: https://www.BrentOzar.com/go/querystack (medium)
+
+Læs mere om single table-queries i Microsofts T-SQL reference:
+
+- https://learn.microsoft.com/en-us/sql/t-sql/queries/select-transact-sql?view=sql-server-ver16
+
+Du kan finde løsninger og svar på opgaverne nederst i scriptet.
+
+*********************** */
+
+USE StackOverflow2013;
+GO
+
+/* ***********************
+
+ØVELSER
+
+*********************** */
+
+/* 
+
+OPGAVE 1: X
+
+- Tabeller involveret:
+- Ønsket output:
+<
+cols
+content
+...
+(X rows)
+>
+
+*/
+
+/* ***********************
+
+LØSNINGER
+
+*********************** */
+
+/* OPGAVE 1 */
+
+/* ***********************
+
+LICENS
+
+Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)
+
+Mere info: https://creativecommons.org/licenses/by-sa/4.0/
+
+Du kan frit:
+
+- Dele: kopiere og distribuere materialet via ethvert medium og i ethvert format
+- Tilpasse: remixe, redigere og bygge på materialet til ethvert formål, selv erhvervsmæssigt
+
+Under følgende betingelser:
+
+- Kreditering: Du skal kreditere, dele et link til licensen og indikere om der er lavet ændringer.
+- Del på samme vilkår: Hvis du remixer, redigerer eller bygger på materialet, så skal dine bidrag
+  distribueres under samme licens som den originale.
+
+*********************** */
